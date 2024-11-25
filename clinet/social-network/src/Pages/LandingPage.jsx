1727f185516ed5c65036c0b143c28d7cf9424a2f
AlpacaFro <31293350+AlpacaFro@users.noreply.github.com>
2024-11-25 11:38:09 +0200

    File Structure update

---
+++ clinet/social-network/src/Pages/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+//components
+import Login from "./Components/Login";
+import Register from "./Components/Register";
+
+
+const LandingPage = () => {
+  return (
+    <div>LandingPage</div>
+  )
+}
+
+export default LandingPage

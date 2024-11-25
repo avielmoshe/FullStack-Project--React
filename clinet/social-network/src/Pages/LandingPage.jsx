b084b122a7c2a1ce293e3ab87b6ef56634731ca5
AlpacaFro <31293350+AlpacaFro@users.noreply.github.com>
2024-11-25 13:04:00 +0200

    fixing routes imports

--- clinet/social-network/src/Pages/LandingPage.jsx
+++ clinet/social-network/src/Pages/LandingPage.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 //components
-import Login from "./Components/Login";
-import Register from "./Components/Register";
 
+
+
+//turnery to check if user is signed if not redirect to here 
 
 const LandingPage = () => {
   return (
-    <div className='background-image'>
-      
+    <div className='bg-slate-400 '>
+      <h1>sss</h1>
     </div>
   )
 }
